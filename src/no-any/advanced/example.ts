df302a1a12d9e1c706382672c3a9e9706c48ba9e
admire-bright <451044731@qq.com>
2023-09-28 08:23:01 +0800

    update type example

--- src/no-any/advanced/example.ts
+++ src/no-any/advanced/example.ts
@@ -1,8 +1,11 @@
 import {AggregationBucketBase} from '../helper';
+import {Hero} from '../middle/keyword';
 
 // 大小写转换
 export type LogType = 'FAIL' | 'RETEST' | 'RETRY' | 'PASS';
 export type LowerLogType = Lowercase<LogType>;
+export type CapitalizeType = Capitalize<LowerLogType>;
+const logtype: CapitalizeType = 'Fail';
 
 // 模板字符串
 export type LogTemplate<T extends string> = `${LogType}_${T}`;
@@ -32,3 +35,28 @@ export interface ListNode<T> {
   value: T;
   next: ListNode<T> | null;
 }
+
+// 组合使用
+type NewHero<T> = {
+  [prop in keyof T as `new${Capitalize<string & prop>}`]-?: T[prop];
+};
+let newHero: NewHero<Hero> = {
+  newHeroJob: {id: '', job: ''},
+  newName: '',
+  newAge: 0,
+  newBoy: false,
+  newMoney: null,
+  newFeature: Symbol('human'),
+  newSkills: [],
+  newHobby: 'eat',
+  newBody: {height: 180, weight: 70},
+  newHelloWorld: function (str?: string | undefined): void {
+    throw new Error('Function not implemented.');
+  },
+  newSetHeroJob: function (): void {
+    throw new Error('Function not implemented.');
+  },
+  newGetHeroJobId: function (defaultId?: string | undefined): string {
+    throw new Error('Function not implemented.');
+  },
+};
--- src/no-any/no-any.d.ts
+++ src/no-any/no-any.d.ts
@@ -3,3 +3,4 @@ declare type StrArrObject = Record<string, string[]>;
 declare type NumArrObject = Record<string, number[]>;
 declare type BaseObject = Record<string, string | string[] | number[] | number>;
 declare type AnyObject = Record<string, any>;
+declare type CustomerObject<T> = Record<string, T>;
